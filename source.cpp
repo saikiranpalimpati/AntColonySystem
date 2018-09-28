2cbfded589bec17782559b014121d860ab831dc8
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-09-28 16:48:48 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	Graph g(10);
-	vector<vector<node>> v;
-	g.addEdge(v, 1, 10);
-	g.displayEdgelist(v);
-	
+	//vector<vector<edge>> v;
+	g.addEdge(0, 1, 10);
+	g.displayEdgelist();
+
 	system("pause");
 }
