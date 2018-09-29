36642e4e2f0e25331411ab15d00606566aa2f8a2
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-09-28 22:21:23 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,12 +1,23 @@
-#include "Graph.cpp"
+#include"Graph.h"
 #include<iostream>
 using namespace std;
 int main()
 {
 	Graph g(10);
-	//vector<vector<edge>> v;
-	g.addEdge(0, 1, 10);
+   g.addEdge(0,1,15);
+   g.addEdge(0,2,10);
+   g.addEdge(0,3,5);
+   g.addEdge(0,4,74);
+   g.addEdge(0,5,44);
+   g.addEdge(0,6,12);
+   g.addEdge(0,7,14);
+   g.addEdge(0,8,45);
+   g.addEdge(2,8,78);
+   g.addEdge(5,4,78);
+   g.addEdge(6,4,7);
+   /* g.addEdge();
+   g.addEdge();
+   g.addEdge();*/
 	g.displayEdgelist();
-
 	system("pause");
 }
