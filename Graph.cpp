114375441cebafe7722c1e66bd7b3e32c87fcab8
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-09-30 00:49:12 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -9,9 +9,7 @@ Graph::Graph(int n)
 
 void Graph::addEdge(int from, int to, int distance)
 {
-
-	edgeList.at(from).push_back(edge(from,to, distance));
-
+	edgeList.at(from).push_back(edge(from, to, distance));
 }
 
 
@@ -19,19 +17,52 @@ void Graph::displayEdgelist()
 {
 	for (auto it : edgeList)
 	{
-		
 		for (auto x : it)
 		{
 			cout << "from node " << x.from;
 			cout << " to" << x.to;
 			cout << "the distance is " << x.distance;
 			cout << endl;
-
-		
 		}
+
 	}
 }
 
+bool Graph::isEdge(int from, int to)
+{
+	bool flag = false;
+
+	for (auto it : edgeList)
+	{
+	  for (auto x : it)
+		{
+		  if ((from == x.from) && (to == x.to))
+			{
+				flag = true;
+				return flag;
+			}
+		}
+	}
+	return flag;
+}
+
+void Graph::removeEdge(int from, int to)
+{
+	int i = 0;
+	for (auto it : edgeList)
+	{
+		for (auto x : it)
+		{
+			if ((from == x.from) && (to == x.to))
+			{
+				int position = i;
+				edgeList[from].erase(edgeList[from].begin()+position);
+			}
+			i++;
+		}
+		i = 0;
+	}	
+}
 
 Graph::~Graph()
 {
