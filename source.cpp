fd1d82e874391af27e16b590fe4023542476e86d
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-10-01 10:44:11 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -15,12 +15,21 @@ int main()
 	g.addEdge(2, 8, 78);
 	g.addEdge(5, 4, 78);
 	g.addEdge(6, 4, 7);
+	g.addEdge(0, 1, 15);
+	g.addEdge(0, 2, 10);
 	g.isEdge(6, 4);
+	
+	cout << "the edge list is" << endl;
+	
 	g.displayEdgelist();
-	g.removeEdge(0, 3);
-	/* g.addEdge();
-	g.addEdge();
-	g.addEdge();*/
+
+	cout << "the edge list after removal of few edges is" << endl;
+	g.removeEdge(7,2 );
+	g.removeEdge(6,4);
+	
+	
 	g.displayEdgelist();
+	cout << g.getWeight(0, 7) << "this is weight "<<endl;
+
 	system("pause");
 }
