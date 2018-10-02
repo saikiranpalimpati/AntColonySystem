629618901c57c8d485ca2495c3a854677758e677
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-10-02 12:02:32 -0500

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,26 +1,29 @@
 #include "Graph.h"
 
 
-
 Graph::Graph(int n)
 {
 	edgeList.resize(n);
+	//initalise the vector with size n 
 }
+
 
 Graph::Graph(const Graph & g)
 {
 	vector<vector<edge>> v = g.edgeList;
+	//copy the vector into another
 }
-
 
 
 void Graph::addEdge(int from, int to, int distance)
 {
-
+	//while adding check wether the edge already exists
 	if (isEdge(from, to))
 	{
 		cout << "edge " << from << " to" << to << "  already exists" << endl;
 	}
+
+	//if the edge does not exist then add the edge 
 	else
 	{
 		edgeList.at(from).push_back(edge(from, to, distance));
@@ -31,8 +34,10 @@ void Graph::addEdge(int from, int to, int distance)
 
 void Graph::displayEdgelist()
 {
+	//traverse trough all the elements in the vector and display them 
 	for (auto it : edgeList)
 	{
+
 		for (auto x : it)
 		{
 			cout << "from node " << x.from;
@@ -40,78 +45,80 @@ void Graph::displayEdgelist()
 			cout << "the distance is " << x.distance;
 			cout << endl;
 		}
-
-	}
-
-
-	cout << "using at" << endl;
-	for (auto x : edgeList.at(1))
-	{
-		cout << x.from << endl;
-		cout << x.to << endl;
-		cout << x.distance << endl;
 	}
 }
+
+
 
 bool Graph::isEdge(int from, int to)
 {
 	bool flag = 0;
 
-	for (auto it : edgeList)
+	//traverse trough the particfular form vector and check wether the edge exists
+	for (auto x : edgeList.at(from))
 	{
-		for (auto x : it)
+		if ((from == x.from) && (to == x.to))
 		{
-			if ((from == x.from) && (to == x.to))
-			{
-				flag = 1;
-				return flag;
-
-			}
+			flag = 1;
+			return flag;
 		}
 	}
+	//return a boolean variable 
 	return flag;
 }
+
+
 
 void Graph::removeEdge(int from, int to)
 {
 	int i = 0;
+	//variable used for incrementation during iteration
+	
 	int position = -1;
+	//variable to catch the position of the edge in the list
 	
-		for (auto it : edgeList)
+	for (auto x : edgeList.at(from))
+	{
+		//traverse and check for the edge in the edgelist
+		if ((from == x.from) && (to == x.to))
 		{
-			for (auto x : it)
-			{
-				if ((from == x.from) && (to == x.to))
-				{
-					position = i;
-					
-				}
-				i++;
-			}
-			i = 0;
+			position = i;
+
 		}
-		if (position >= 0)
-		{
-			edgeList[from].erase(edgeList[from].begin() + position);
-		}
-	
+		i++;
+	}
+	i = 0;
+
+	//if the position value changed then at that partuicular position erase the edge
+	if (position >= 0)
+	{
+		edgeList[from].erase(edgeList[from].begin() + position);
+
+	}
+
+	//if the position is not changed then it means it does not exist
+	else
+	{
+		cout << "edge doesnot exist so it cannot be removed" << endl;
+	}
+
 }
 
 int Graph::getWeight(int from, int to)
 {
 	int weight = 0;
-
-	for (auto it : edgeList)
+	//having a varible to return edge weight
+	
+	for (auto x : edgeList.at(from))
 	{
-		for (auto x : it)
+		if (x.to == to)
 		{
 			weight = x.distance;
-
 		}
 
 	}
-	return weight;
 
+	return weight;
 }
 
 
