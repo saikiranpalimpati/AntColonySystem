38da5e063e90b4dcb09688d9b80fad5884db5fd4
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-11-16 09:54:28 -0600

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,5 +1,4 @@
-//#include"Graph.h"
-#include"colony.h"
+#include"ant.h"
 #include<iostream>
 #include<fstream>
 using namespace std;
@@ -16,38 +15,12 @@ int main()
 	{
 		g->addEdge(from, to, distance);
 	}
+
+	c.initialisePheromone(*g);
+	Ant a;
+	a.initialiseAnts(*g);
+	a.antTour(*g);
+	a.display();
 	
-	//c.displayGraph(* g);
-	
-	c.greedyPath(*g,1);
-	//g->displayEdgelist();
-
-	//g->removeEdge(0, 2);
-	
-	/*
-	//checking wether the defined edge exists or not
-	g.isEdge(6, 4);
-
-	//displaying the edgelist
-	g.displayEdgelist();
-
-	g.changePheromone(0, 1, 5);
-
-	cout << endl;
-
-	g.displayEdgelist();
-
-	//removing few edges from edgelist
-	//g.removeEdge(0, 2);
-	//g.removeEdge(0, 4);
-
-	//displaying the edgelist after removing few edges
-	//g.displayEdgelist();
-
-	//get weight of on of the edge
-	cout << g.getWeight(0, 2) << endl;
-	*/
-
-
 	system("pause");
 }
