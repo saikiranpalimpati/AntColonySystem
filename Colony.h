34fedc17b7cdcbe89733421563048480324507a3
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-11-16 09:52:42 -0600

    Update Colony.h

--- Colony.h
+++ Colony.h
@@ -1,12 +1,25 @@
+/*
+colony class can display the graphs in it and 
+can find the cost of path with greedy algortihm
+it can initialise the pheromone accordingly
+*/
 #pragma once
 #include"Graph.h"
 class Colony
 {
 	Graph *Gph;
+	//variable to access the graph class
 public:
 	Colony();
+	//constructor
 	void displayGraph(Graph&);
+	//method to display folloing graph
 	double greedyPath(Graph&,double);
+	//method to calculate the cost with greedy algortihm
+	void initialisePheromone(Graph&);
+	//method to initialise pheromone
 	~Colony();
+	//destructor
 };
+
 
