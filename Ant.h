61604a19a44d00fb30ba7ac4f127aed57f9c1e5a
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2018-11-26 11:10:04 -0600

    Update Ant.h

--- Ant.h
+++ Ant.h
@@ -22,18 +22,35 @@ class Ant
 public:
 	//constructor
 	Ant();
+	
 	//method to initialise the properties of ants
 	void initialiseAnts(Graph&);
+	
+	//method to return the number of ants
+	void updateAnt(double,double);
+	
 	//method to return the number of ants
 	int returnNumberOfAnts();
+	
 	//method to display all the ants
 	void displayAnts();
+	
 	//method to make a tour trough the graph
 	void antTour(Graph&);
+	
 	//method to get thr next vertex of an ant
 	double getProbability(Graph&,double,double);
+	
+	//method that evouporates the pheromone on edges
+	void evouparatePheromone(Graph&);
+	
+	//method to update the pherone after the ant tour
+	void updatePheromone(Graph&,double,double);
+	
+	//method to display the ant tour
+	void displayPath();
+	
 	//destructor
-	void display();
 	~Ant();
 };
 
