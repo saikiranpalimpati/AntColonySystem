a542453712fa5001038d169f4d149b0f986ada57
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-01-17 12:00:39 -0600

    Update Ant.h

--- Ant.h
+++ Ant.h
@@ -1,56 +1,23 @@
-/*
-Ant class will create ants and initialise
-pheomone its current positon its previous position
-and the list of covered nodes in a vector 
-*/
 #pragma once
-#include "Colony.h"
+#include "Graph.h"
 //structure to bind all the members of an ant
-struct ant
-{
-	double pheromone;
-	double currentPosition;
-	double previousPosition;
-	vector<double> nodeMemory;
-};
 
 class Ant
 {
-	//vector to hold the ants
-	vector<ant> ant;
-	Graph *gp;
+	Graph *Gph;
+	int index;
+	double pheromone;
+	int position;
+	vector<int> path;
 public:
-	//constructor
-	Ant();
-	
-	//method to initialise the properties of ants
-	void initialiseAnts(Graph&);
-	
-	//method to return the number of ants
-	void updateAnt(double,double);
-	
-	//method to return the number of ants
-	int returnNumberOfAnts();
-	
-	//method to display all the ants
-	void displayAnts();
-	
-	//method to make a tour trough the graph
-	void antTour(Graph&);
-	
-	//method to get thr next vertex of an ant
-	double getProbability(Graph&,double,double);
-	
-	//method that evouporates the pheromone on edges
-	void evouparatePheromone(Graph&);
-	
-	//method to update the pherone after the ant tour
-	void updatePheromone(Graph&,double,double);
-	
-	//method to display the ant tour
+	Ant(Graph&,int,double,int,int);
+	int displayIndexNumber();
+	int getTheCurrentPosition();
+	int nextPostionByNearestNeighbour(int,int);
+	double displayPheromone();
+	vector<int> returnPath();
 	void displayPath();
-	
-	//destructor
+	void updatePosition(int);
+	//int findNextVertex(Graph &g);
 	~Ant();
 };
-
