5b263642e67f369d55eb90a3f0c65ffbf854847c
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-01-22 16:08:52 -0600

    Update Ant.h

--- Ant.h
+++ Ant.h
@@ -1,3 +1,6 @@
+/* The ant class has some data for each ant such as pheromone 
+ its positon. the path it has travelled previously and the distance 
+  travelled by it */
 #pragma once
 #include "Graph.h"
 //structure to bind all the members of an ant
@@ -5,21 +8,48 @@
 class Ant
 {
 	Graph *Gph;
+
+	//unique index for each ant
 	int index;
+	//poheromone level of the ant
 	double pheromone;
+	//position of the ant on a graph
 	int position;
+	//vector to track the path of the ant
 	vector<int> path;
+	//distance travelled by the ant
 	double distanceTravelled;
+
 public:
+	//ant constructor
 	Ant(Graph&, int, double);
+
+	//method to display the ant index number
 	int displayIndexNumber();
+	
+	//method to display the current position 
 	int getTheCurrentPosition();
+
+	//method to find the next positon of the ant
 	int nextPostionByNearestNeighbour(int, int);
+
+	//method to display the pheromone level of the ant
 	double displayPheromone();
+
+	//method to return the path
 	vector<int> returnPath();
+
+	//method to display the ant path
 	void displayPath();
+
+	//method to update the position of the ant 
 	void updatePosition(int);
-	void updateDistance(int,int);
+
+	//method to update the distance  travelled by the ant
+	void updateDistance(int, int);
+
+	//method to display the distance travelled by the ant
 	double displayDistanceTravelled();
+
 	~Ant();
 };
