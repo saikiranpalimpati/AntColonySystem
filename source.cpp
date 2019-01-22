a819d9f7ba9e88b866ab1f774ebbd69bdb3d42d7
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-01-22 14:52:08 -0600

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,27 +1,38 @@
-
 #include"Ant.h"
 #include"Colony.h"
 #include<fstream>
+//header files
+
 
 int main()
 {
 	ifstream infile;
+	//input file variable
+
 	infile.open("distanceList.txt");
+	//open input file
 
 	int from, to;
 	double distance;
-	Graph * g=new Graph(100);
+	//variables
+
+	Graph * g = new Graph(100);
 	
-	
+	//reading the input file
 	while (infile >> from >> to >> distance)
 	{
 		g->addEdge(from, to, distance);
 	}
+
+	//initialise colony
 	Colony c(*g);
-	//g->displayEdgelist();
-	c.initialiseAnts(*g);
+
+	//initilise ants in a colony
+	c.initialiseAnts();
+
+	//make ants to make a tour
+	c.AnttourUsingNearestNeighbourAlgorithm(3);
+
 	//c.displayAnts();
-	//cout << g->returnNumberOfVertices() << endl;;
-	c.AnttourUsingGreddyALgorithm();
 	system("pause");
 }
