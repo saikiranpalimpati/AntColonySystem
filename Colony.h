80808cf79e86747c4897c1476e0e84ad0e22db50
saikiranpalimpati <34260562+saikiranpalimpati@users.noreply.github.com>
2019-01-22 16:09:33 -0600

    Update Colony.h

--- Colony.h
+++ Colony.h
@@ -1,5 +1,5 @@
 /*colony class will take a graph and hold the individual ants.
-It makes the ant to move around the graph and tour trough all the cities(or vertices) 
+It makes the ant to move around the graph and tour trough all the cities(or vertices)
 on the graph using different algorithms.
 */
 #pragma once
@@ -12,14 +12,16 @@ class Colony
 	//graph variable
 	vector<Ant> Ants;
 	//vector to hold the ant objects
+
 public:
+
 	//initilaising the graph in a colony
 	Colony(Graph&);
-	
+
 	//initilaising the ants
 	void initialiseAnts();
 
-	//dispalyi the ant attributes
+	//dispalying the ant attributes
 	void displayAnts();
 
 	//make ants tour around the graph by using nearest neighbour algorithm
